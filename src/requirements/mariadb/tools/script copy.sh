6f8f53fff0c6ae3ef06bb9648634dca5f4113440
Jules <jbjulesbernard3@gmail.com>
2024-03-25 16:58:11 +0100

    norm

--- src/requirements/mariadb/tools/script copy.sh
+++ src/requirements/mariadb/tools/script copy.sh
@@ -1,3 +1,14 @@
+service mysql start
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqld_safe
+
+
+
 set -a
 source .env
 set +a
@@ -6,27 +17,26 @@ service mysql start;
 # create a database (if the database does not exist)
 echo "1"
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-sleep 2
 
 # create an user with a password (if the user does not exist)
 echo "2"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-sleep 2
+
 # give all privileges to the user
 echo "3"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-sleep 2
+
 #modify sql database
 echo "4"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-sleep 2
+
 #reload the database
 echo "5"
-sleep 2
+mysql -e "FLUSH PRIVILEGES;"
+
 #shutdown
 echo "6"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-sleep 2
+
 #use exec to 
 echo "7"
-#exec mysqld_safe
--- src/requirements/wordpress/conf/wp-config-cli.sh
+++ src/requirements/wordpress/conf/wp-config-cli.sh
@@ -1,20 +1,26 @@
-sleep 10
-
+#sleep 30
+set -a
+source .env
+set +a
+echo "--------"
 wp config create --allow-root \
                  --dbname=$SQL_DATABASE \
                  --dbuser=$SQL_USER \
                  --dbpass=$SQL_PASSWORD \
                  --dbhost=mariadb:3306 --path='/var/www/wordpress'
-
-wp core install --url=$DOMAIN \
+echo "--------"
+wp core install --allow-root \
+                --url=$DOMAIN \
                 --title=inception \
                 --admin_user=$WP_ADMIN \
                 --admin_password=$WP_ADMIN_PW \
-                --admin_email=info@example.com
-
-wp user create $WP_USER2 $WP_USER2@example.com \
-					--role=author
-
+                --admin_email=info@example.com \
+                --path='/var/www/wordpress'
+echo "--------"
+wp user create --allow-root $WP_USER2 $WP_USER2@example.com \
+					--role=author \
+                    --path='/var/www/wordpress'
+echo "--------"
 if [ ! -d /run/php ]; then
     mkdir ./run/php
 fi
